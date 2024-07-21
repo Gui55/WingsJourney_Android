88d37b9f295f0d8843a4514b06b9e1609314adb5
guilhermemedeirosmergulhao <gui.mergulhao55@outlook.com>
2024-07-21 13:54:35 -0300

    Testes unitários para o repositórios

--- app/src/test/java/com/example/wingsjourney/login/usecase/LoginUseCaseImplTest.kt
+++ app/src/test/java/com/example/wingsjourney/login/usecase/LoginUseCaseImplTest.kt
@@ -1,15 +1,19 @@
 package com.example.wingsjourney.login.usecase
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.core.usecase.base.ResultStatus
 import com.example.testingutils.MainCoroutineRule
 import com.example.wingsjourney.login.data.repository.LoginRepository
 import com.example.wingsjourney.login.framework.network.request.LoginRequest
 import com.example.wingsjourney.login.framework.network.response.LoginResponse
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -29,53 +33,116 @@ class LoginUseCaseImplTest {
     @Mock
     private lateinit var loginRepository: LoginRepository
 
-    @Test
-    fun `check positive login response doesn't return empty token or null`() =
-        runTest{
+    private lateinit var loginUseCase: LoginUseCase
 
+    @Before
+    fun setup() {
+        loginUseCase = LoginUseCaseImpl(loginRepository)
+    }
+
+    @Test
+    fun `check if useCase really starts with loading status`() =
+        runTest{
             //Arrange
             whenever(loginRepository.login(any()))
-            .thenReturn(
-                LoginResponse("token")
-            )
+                .thenReturn(LoginResponse("token"))
 
             //Act
-            val result = loginRepository.login(LoginRequest("user", "password"))
+            val result = loginUseCase(LoginUseCase.LoginParams(LoginRequest("user", "password")))
+                .toList()
 
             //Assert
-            assert(result.token.isNotEmpty())
-            assertNotNull(result)
+            assert(result[0] is ResultStatus.Loading)
         }
 
     @Test
-    fun `check empty token login response`() = runTest {
+    fun `check if repository is really being called`() =
+        runTest {
 
-        //Arrange
-        whenever(loginRepository.login(any()))
-            .thenReturn(
-                LoginResponse("")
-            )
+            //Arrange
+            val request = LoginRequest("user", "password")
 
-        //Act
-        val result = loginRepository.login(LoginRequest("user", "password"))
+            whenever(loginRepository.login(request))
+                .thenReturn(LoginResponse("specific"))
 
-        //Assert
-        assert(result.token.isEmpty())
+            //Act
 
-    }
+            /* Segundo a documentação oficial do Android: Se o teste precisar conferir vários
+            valores, chamar toList() vai fazer com que o fluxo aguarde a fonte emitir todos os
+            valores e, em seguida, vai retornar esses valores como uma lista */
 
-    @Test(expected = RuntimeException::class)
-    fun `check repository method returning exception`() = runTest {
+            //Sendo assim, o método do repositório mockado só vai ser chamado no teste com o uso
+            //do método toList()
 
-        //Arrange
-        whenever(loginRepository.login(any()))
-            .thenThrow(
-                RuntimeException("error")
-            )
+            loginUseCase(LoginUseCase.LoginParams(request)).toList()
 
-        //Act
-        loginRepository.login(LoginRequest("user", "password"))
+            //Assert
+            verify(loginRepository, times(1)).login(request)
+        }
 
-    }
+    @Test
+    fun `check useCase returning a success status`() =
+        runTest {
+
+            //Arrange
+            whenever(loginRepository.login(any()))
+                .thenReturn(LoginResponse("token"))
+
+            //Act
+            val result = loginUseCase(LoginUseCase.LoginParams(LoginRequest("user", "password")))
+                .toList()
+
+            //Assert
+            assert(result[1] is ResultStatus.Success)
+
+        }
+
+    @Test
+    fun `check useCase returning a success status even with empty token`() =
+        runTest {
+
+            //Arrange
+            whenever(loginRepository.login(any()))
+                .thenReturn(LoginResponse(""))
+
+            //Act
+            val result = loginUseCase(LoginUseCase.LoginParams(LoginRequest("user", "password")))
+                .toList()
+
+            //Assert
+            assert(result[1] is ResultStatus.Success)
+
+        }
+
+    @Test
+    fun `check if useCase really returns loading, and later, a second status`() =
+        runTest {
+            //Arrange
+            whenever(loginRepository.login(any()))
+                .thenReturn(LoginResponse(""))
+
+            //Act
+            val result = loginUseCase(LoginUseCase.LoginParams(LoginRequest("user", "password")))
+                .toList()
+
+            //Assert
+            assert(result[0] is ResultStatus.Loading)
+            assert(result[1] is ResultStatus.Success)
+        }
+
+    @Test
+    fun `check useCase returning error`() =
+        runTest {
+            //Arrange
+            whenever(loginRepository.login(any()))
+                .thenThrow(RuntimeException("error"))
+
+            //Act
+            val result = loginUseCase(LoginUseCase.LoginParams(LoginRequest("user", "password")))
+                .toList()
+
+            //Assert
+            assert(result[1] is ResultStatus.Error)
+        }
 
 }
